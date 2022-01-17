55ec060e150d8d4acef87ce1af1d6ba364187e82
vitorsoaresf <vitorsoaresf@gmail.com>
2022-01-17 08:01:46 -0300

    feat: implements context Auth

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { theme } from "../styles/theme";
+import { AuthProvider } from "./Auth.Context";
 
 interface AppProviderProps {
   children: ReactNode;
 }
 
 export const AppProvider = ({ children }: AppProviderProps) => (
-  <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  <AuthProvider>
+    <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  </AuthProvider>
 );
