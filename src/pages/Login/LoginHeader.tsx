126cd119b26ddd7d0c14361ff87467041b54044c
vitorsoaresf <vitorsoaresf@gmail.com>
2022-01-19 07:58:28 -0300

    feat: implements header

--- src/pages/Login/LoginHeader.tsx
+++ src/pages/Login/LoginHeader.tsx
@@ -3,20 +3,33 @@ import { FiShoppingBag } from "react-icons/fi";
 
 export const LoginHeader = () => {
   return (
-    <VStack>
-      <Heading ml="16px" display="flex" alignItems="center" fontSize={["2xl"]}>
-        Burguer{" "}
-        <Text
-          h="100%"
-          ml="5px"
-          color="#EB5757"
-          fontSize={["sm"]}
-          fontWeight="700"
-        >
-          Kenzie
-        </Text>
-      </Heading>
-      <Flex border="1px" borderColor="gray.100" w="90vw">
+    <VStack w="100vw" maxW="377px" m="0" display="flex">
+      <Flex w="90%" m="50px 0px 10px 0">
+        <Flex fontSize={["2xl"]} alignItems="flex-end">
+          <Heading display="flex" alignItems="flex-end">
+            Burguer
+          </Heading>
+          <Heading
+            h="100%"
+            m="0px 0px 5px 5px"
+            color="#EB5757"
+            fontSize={["md"]}
+            fontWeight="700"
+            display="flex"
+            alignItems="flex-end"
+          >
+            Kenzie
+          </Heading>
+        </Flex>
+      </Flex>
+      <Flex
+        border="1px"
+        borderColor="gray.100"
+        w="90%"
+        h="95px"
+        alignItems="center"
+        justifyContent="center"
+      >
         <Center
           bg="green.200"
           color="green.800"
@@ -26,7 +39,7 @@ export const LoginHeader = () => {
         >
           <FiShoppingBag />
         </Center>
-        <Text fontSize="14px" color="gray.300">
+        <Text fontSize="14px" w="200px" color="gray.300" ml="5px">
           A vida é como um sanduíche, é preciso recheá-la com os{" "}
           <strong>melhores </strong>
           ingredientes.
